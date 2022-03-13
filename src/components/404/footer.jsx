d029124e6ec96c1044f390b6b3243dc28bfad8ad
Rajkumar-justcoder <meet.r.graphicat10@gmail.com>
2022-03-13 17:49:02 +0530

    Fix: Link in footer

--- src/components/404/footer.jsx
+++ src/components/404/footer.jsx
@@ -5,7 +5,7 @@ function footer() {
         <>
             <footer class="footer mt-auto py-3 ">
                 <div class="container">
-                    <span class="text-muted">created by <a href="https://github.com/rajkumar-justcoder">Rajkumar Khistariya | JustCoder</a> - devChallenges.io</span>
+                    <span class="text-muted">created by <a href="https://github.com/rajkumar-justcoder">Rajkumar Khistariya | JustCoder</a> - <a href="https://devchallenges.io/">devChallenges.io</a> </span>
                 </div>
             </footer>
         </>
